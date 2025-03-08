54e543017c4a81492f192856c21c369d2873f364
miketeow <27071053+miketeow@users.noreply.github.com>
2025-03-09 00:56:14 +0800

    add docker file, typesafe env file

--- src/auth/actions.ts
+++ src/auth/actions.ts
@@ -1,6 +1,11 @@
+"use server";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 import { signInSchema, signUpSchema } from "./schema";
+import { db } from "@/drizzle/db";
+import { eq } from "drizzle-orm";
+import { UserTable } from "@/drizzle/schema";
+import { generateSalt, hashPassword } from "./core/password-hashser";
 
 export async function signIn(unsafeData: z.infer<typeof signInSchema>) {
   const { success, data } = signInSchema.safeParse(unsafeData);
@@ -18,6 +23,18 @@ export async function signUp(unsafeData: z.infer<typeof signUpSchema>) {
     console.log(data);
     return "Unable to create account";
   }
+
+  const existingUser = await db.query.UserTable.findFirst({
+    where: eq(UserTable.email, data.email),
+  });
+
+  if (existingUser != null) return "Account already exists for this email";
+
+  const hashedPassword = await hashPassword(data.password, generateSalt());
+  console.log("testing here");
+
+  console.log(hashedPassword);
+
   redirect("/");
 }
 export async function logOut() {
